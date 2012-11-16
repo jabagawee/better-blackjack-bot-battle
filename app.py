a76121c531c6e105c2ac6ef526d49897cef3478f
Andrew Szeto <andrew@jabagawee.com>
2012-11-16 01:27:05 -0800

    registering api tokens

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 #! /usr/bin/env python
 
+import datetime
 import os
+import random
 
 import pymongo
 from flask import Flask
 
 connection = pymongo.Connection(host=os.environ['MONGOLAB_URI'])
 db = connection[os.environ['MONGOLAB_DATABASE']]
+auth_collection = db['auth']
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def root():
     return "Hello World"
+
+valid_chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+
+@app.route('/api/register/<username>')
+def register(username):
+    entry = auth_collection.find_one({'username': username})
+    if entry:
+        return 'Already registered. Your token is %s' % (entry['token'])
+    token = ''.join(random.choice(valid_chars) for __ in xrange(25))
+    auth = {'username': username,
+            'token': token,
+            'created_time': datetime.datetime.utcnow()}
+    auth_collection.insert(auth)
+    return "Registered. Your token is %s" % (token)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
