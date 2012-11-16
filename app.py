f13f30cfdf0688d208f9a7acc544bf18e9bbb9b5
Andrew Szeto <andrew@jabagawee.com>
2012-11-16 01:14:31 -0800

    no more debugging info needed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World, %s, %s" % (connection, db)
+    return "Hello World"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
