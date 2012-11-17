56689b423e246c961534f4079372f8a3eba3b846
Andrew Szeto <andrew@jabagawee.com>
2012-11-16 16:49:55 -0800

    made some of the API for gaming

--- app.py
+++ app.py
@@ -3,12 +3,11 @@
 import datetime
 import os
 import random
+from functools import wraps
 
 import pymongo
-from flask import Flask, jsonify, request, url_for
+from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
-
-import documentation
 
 connection = pymongo.Connection(host=os.environ['MONGOLAB_URI'])
 db = connection[os.environ['MONGOLAB_DATABASE']]
@@ -19,13 +18,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+    username = None
     if 'apikey' in request.args:
         apikey = request.args['apikey']
         document = auth_collection.find_one({'apikey': apikey})
-        username = document['username']
-    else:
+        if document:
+            username = document['username']
+            in_game = document['in_game']
+            cash = document['cash']
+    if username is None:
         username = "Anonymous"
-    return "Hello, %s." % (username)
+        in_game = False
+        cash = 0
+    if in_game:
+        return "Hello, %s. \
+                You are currently in a game and you have $%d." \
+                % (username, cash)
+    return "Hello, %s. \
+            You are currently not in a game and you have $%d." \
+            % (username, cash)
 
 
 def generate_apikey():
@@ -33,7 +44,7 @@ def generate_apikey():
     return ''.join(random.choice(valid_chars) for __ in xrange(25))
 
 
-@app.route('/api/get_apikey')
+@app.route('/api/keys/get')
 def get_apikey():
     if 'username' not in request.args or 'password' not in request.args:
         return jsonify({'success': False,
@@ -52,10 +63,64 @@ def get_apikey():
     document = {'username': username,
                 'pass_hash': generate_password_hash(password),
                 'apikey': apikey,
-                'created_time': datetime.datetime.utcnow()}
+                'created_time': datetime.datetime.utcnow(),
+                'cash': 5000,
+                'in_game': False}
     auth_collection.insert(document)
     return jsonify({'success': True, 'apikey': apikey})
 
+
+def valid_apikey_required(view):
+    @wraps(view)
+    def decorated_view(*args, **kwargs):
+        if 'apikey' not in request.args:
+            return jsonify({'success': False, 'error': 'No apikey found.'})
+        apikey = request.args['apikey']
+        document = auth_collection.find_one({'apikey', apikey})
+        if document:
+            return view(*args, **kwargs)
+        return jsonify({'success': False, 'error': 'Invalid apikey.'})
+    return decorated_view
+
+
+@app.route('/api/keys/check')
+@valid_apikey_required
+def check_apikey():
+    document = auth_collection.find_one({'apikey', request.args['apikey']})
+    return jsonify({'success': True,
+                    'username': document['username'],
+                    'cash': document['cash'],
+                    'in_game': document['in_game']})
+
+
+@app.route('/api/game/new')
+@valid_apikey_required
+def new_game():
+    pass
+
+
+@app.route('/api/game/hit')
+@valid_apikey_required
+def hit():
+    pass
+
+
+@app.route('/api/game/stand')
+@valid_apikey_required
+def stand():
+    pass
+
+
+@app.route('/api/game/double_down')
+@valid_apikey_required
+def double_down():
+    pass
+
+
+@app.route('/api/game/surrender')
+@valid_apikey_required
+def surrender():
+    pass
 
 
 if __name__ == "__main__":
